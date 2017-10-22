b7ca24843325121629df456aae563fca5f9af6d1
Julia Rossi <julia.rossi@gmail.com>
2017-10-21 19:10:37 -0700

    start story form

--- src/components/main.js
+++ src/components/main.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import HelloWorld from './hello-world'
 import Map from './map'
+import Story from './story'
 
 const Main = () => (
   <main>
     <Switch>
       <Route exact path='/' component={HelloWorld}/>
       <Route exact path='/map' component={Map}/>
+      <Route exact path='/story' component={Story}/>
     </Switch>
   </main>
 )
