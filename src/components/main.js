1ee3b8cd42de2db1460c9d8c54995afc9fdc7ef1
Shibo Yao <shibo.yao@gmail.com>
2017-10-22 11:15:50 -0700

    Load data from api + heatmap

--- src/components/main.js
+++ src/components/main.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import HelloWorld from './hello-world'
-import Map from './map'
+import JMap from './map'
 import Story from './story'
 
 const Main = () => (
   <main>
     <Switch>
       <Route exact path='/' component={HelloWorld}/>
-      <Route exact path='/map' component={Map}/>
+      <Route exact path='/map' component={JMap}/>
       <Route exact path='/story' component={Story}/>
     </Switch>
   </main>
--- src/components/map.js
+++ src/components/map.js
@@ -1,68 +1,68 @@
 import React, { Component } from 'react';
-import ReactMapGL from 'react-map-gl';
-import Button from './button';
+import { Map, TileLayer } from 'react-leaflet';
+import HeatmapLayer from 'react-leaflet-heatmap-layer';
 import '../assets/css/app.css';
+import 'leaflet/dist/leaflet.css';
 
-const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
-
-class Map extends Component {
+class JMap extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-    viewport: {
-      latitude: 32.7157,
-      longitude: -117.1611,
-      zoom: 11,
-      bearing: 0,
-      pitch: 0,
-      // width: 1280,
-      // height: 720
-    },
-    settings: {
-      dragPan: true,
-      dragRotate: true,
-      scrollZoom: true,
-      touchZoomRotate: true,
-      doubleClickZoom: true,
-      trackResize: true,
-      minZoom: 0,
-      maxZoom: 20,
-      minPitch: 0,
-      maxPitch: 85
-    },
-    backgroundColor: 'green'
-  }
-  
-  this.changeBackgroundColor = this.changeBackgroundColor.bind(this);
-  
-}
-  
-  
-  changeBackgroundColor(){
-    this.setState({
-      backgroundColor: 'blue',
-    })
+      viewport: {
+        center: [32.7157, -117.1611],
+        zoom: 13
+      },
+      heatmap: {
+        points: []
+      }
+    }
   }
 
-  _onViewportChange = viewport => this.setState({viewport});
+  componentDidMount() {
+    fetch('http://52.53.201.52:8000/api', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }).then((response)=>{
+      return response.json();
+    }).then((data) => {
+      let points = [];
+      for (let i = 0; i < data.length; i++) {
+        let lat = parseFloat(data[i].latitude);
+        let long = parseFloat(data[i].longitude);
+        if (!isNaN(lat) && !isNaN(long)) {
+          points.push([lat, long, '5']);
+        }
+      }
+      this.setState({
+        heatmap: {
+          points: points
+        }
+      });
+    });
+  }
 
   render() {
-    const {viewport, settings} = this.state;
+    const {viewport, heatmap} = this.state;
 
     return (
-      <div>
-        <Button backgroundColor={this.state.backgroundColor} changeBackgroundColor={this.changeBackgroundColor} />
-        Woah: {this.state.viewport.pitch}
-      <ReactMapGL className="Map-container"
-        {...viewport}
-        {...settings}
-        mapStyle='mapbox://styles/mapbox/light-v9'
-        mapboxApiAccessToken={MAPBOX_TOKEN}
-        onViewportChange={this._onViewportChange}
-      />
-    </div>
-    );
-  }
+      <div className="Map-container">
+        <Map {...viewport} >
+        <TileLayer
+          url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>' />
+        <HeatmapLayer
+          points={heatmap.points}
+          longitudeExtractor={m => m[1]}
+          latitudeExtractor={m => m[0]}
+          intensityExtractor={m => parseFloat(m[2])} />
+        </Map>
+      </div>
+      );
+    }
 }
 
-export default Map
+export default JMap
