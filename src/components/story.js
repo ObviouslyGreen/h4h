15107a83e63b011814bcbb239951d8ada59dbad9
Jordan Duncan <j.duncan@rochester.edu>
2017-10-22 02:48:34 -0700

    testing api call

--- src/components/story.js
+++ src/components/story.js
@@ -26,8 +26,23 @@ class Story extends Component {
   }
 
   handleChange(event) {
-    alert(event.target.value);
-    this.setState({gender: event.target.value});
+    console.log(event.target.id);
+    let key = event.target.id;
+    let value = event.target.value;
+    let object = {};
+    object[key] = value;
+    console.log(key)
+    this.setState(object);
+    fetch('http://52.53.201.52:5000/api/newstory', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: object
+    }).then((response)=>{
+      console.log(response);
+    })
   }
 
 
@@ -43,14 +58,14 @@ class Story extends Component {
           <form onSubmit={this.handleSubmit}>
           <label>
           On <DatePicker selected={this.state.startDate}/>, I experienced/witnessed an incident of 
-          <select name="event_type" value={this.state.event_type} onChange={this.handleChange}>
+          <select name="event_type" id="event_type" value={this.state.event_type} onChange={this.handleChange}>
                 <option value="harrassment">harrassment</option>
                 <option value="assault"> assault</option>
                 <option value="sexual assault">sexual assault</option>
                 <option value="workplace harassment">workplace harassment</option>
                 <option value="micro-aggeression">micro-aggeression</option>
             </select>. The incident occurred at <input type="text" name="location" onChange={this.handleChange}/>, which is a 
-          <select name="location_type" value={this.state.location_type} onChange={this.handleChange}>
+          <select name="location_type" id="location_type" value={this.state.location_type} onChange={this.handleChange}>
               <option value="school/college">school/college</option>
               <option value="work">work</option>
               <option value="park">public park</option>
@@ -59,13 +74,13 @@ class Story extends Component {
           </select>. 
 
           I believe it was driven by my gender and I identify as           
-          <select name="gender" value={this.state.gender} onChange={this.handleChange}>
+          <select name="gender" id="gender" value={this.state.gender} onChange={this.handleChange}>
               <option value="female">female</option>
               <option value="male">male</option>
               <option value="other">other</option>
               <option value="neither">neither</option>
           </select>. I 
-          <select name="knowAssailant" value={this.state.knowAssailant} onChange={this.handleChange}>
+          <select name="knowAssailant" id="knowAssailant" value={this.state.knowAssailant} onChange={this.handleChange}>
               <option value="true">know</option>
               <option value="false">do not know</option>
           </select> my assailant.
@@ -74,10 +89,12 @@ class Story extends Component {
               <option value="true">have</option>
               <option value="false">have not</option>
           </select> report this to the authorities.  
-              <input type="submit" value="Tell my story" />
+              <input type="submit" value="Tell myasdfasd story" />
               </label>
           </form>
           <a>{this.state.gender}</a>
+          <a>{this.state.event_type}</a>
+          <a>{this.state.location_type}</a>
       </div>
     );
   }
